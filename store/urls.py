cc45c893461aa9fba983a0fdfd97c4780b3d50af
skantor <serhiykantor@gmail.com>
2022-12-30 02:25:31 +0200

    fix some visual bugs

--- store/urls.py
+++ store/urls.py
@@ -23,6 +23,6 @@ from products.views import index
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index"),
-    path("products", include("products.urls", namespace="products")),
-    path("users", include("users.urls", namespace="users")),
+    path("products/", include("products.urls", namespace="products")),
+    path("users/", include("users.urls", namespace="users")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
