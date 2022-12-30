baf0f3b1c3a4642366ce4d48e0d65dd74c749a78
skantor <serhiykantor@gmail.com>
2022-12-30 17:05:03 +0200

    add login logic and page

---
+++ users/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control py-4",
+                "placeholder": "Input username",
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control py-4",
+                "placeholder": "Input password",
+            }
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ["username", "password"]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+
+from users.forms import UserLoginForm
 
 
 def login(request):
-    return render(request, "users/login.html")
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect("/")
+
+    else:
+        form = UserLoginForm()
+    context = {
+        "form": form
+    }
+    return render(request, "users/login.html", context=context)
 
 
 def register(request):
